ed55deca04fce582bf820c2846686185e6fabc8e
Sharika <googly.123@outlook.com>
2018-08-03 14:47:33 -0700

    Uploading Another one

---
+++ script.js
@@ -0,0 +1,29 @@
+const maryTimeline = () => {
+	const newsfeed = "Ooooh I heard that some of Sharika's Family Member's Are Checking This Crap Out!";
+	alert("Mary Says: " + newsfeed);
+}
+
+const andrewTimeline = () => {
+	const newsfeed = "Lol Lol You Know I kinda ship Arshi and Arshad But I do kinda Ship Arshi and Anfal";
+	alert("Andrew Says: " + newsfeed);
+}
+
+const sharikaTimeline = () => {
+	const newsfeed = "Hey I Created This Website!";
+	alert("Sharika Says: " + newsfeed);
+}
+
+const andreiTimeline = () => {
+	const newsfeed = "I'm the instructor of The course That Sharika uses to make websites now";
+	alert("Sharika Says: " + newsfeed);
+}
+
+const matthewTimeline = () => {
+	const newsfeed = "This is Crappy Facebook... I'm Matthew And You Must Be Someone Related To Sharika!";
+	alert("Matthew Says: " + newsfeed);
+}
+
+const veronicaTimeline = () => {
+	const newsfeed = "I am Veronica From Riverdale!";
+	alert("Veronica Says: " + newsfeed);
+}
